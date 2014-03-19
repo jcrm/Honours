2d9ae58f08e8890237a7d24e44c28f992507ec7a
jakemorey@hotmail.co.uk <ctm5010>
2014-03-19 13:32:36 +0000

    only one left then hopefully it will work

--- Honours/Shader/cloud.cu
+++ Honours/Shader/cloud.cu
@@ -40,7 +40,7 @@ __global__ void cuda_kernel_divergence(unsigned char* divergence, unsigned char*
 	int zIter = 0;
 
 	// Get velocity values from neighboring cells.  
-	unsigned char *fieldLeft = velocityInput + (zIter*pitchSlice) + (yIter*pitch) + (4*(xIter-1));  
+	unsigned char *fieldLeft = velocityInput + (zIter*pitchSlice) + (yIter*pitch) + (4*(xIter-1));
 	unsigned char *fieldRight = velocityInput + (zIter*pitchSlice) + (yIter*pitch) + (4*(xIter+1));
 	unsigned char *fieldBottom = velocityInput + ((zIter+1)*pitchSlice) + (yIter*pitch) + (4*xIter);  
 	unsigned char *fieldTop = velocityInput + ((zIter-1)*pitchSlice) + (yIter*pitch) + (4*xIter);   
@@ -50,20 +50,26 @@ __global__ void cuda_kernel_divergence(unsigned char* divergence, unsigned char*
 	divergence[0] =  0.5 * ((fieldRight[0] - fieldLeft[0])+  
 								(fieldTop[1] - fieldBottom[1])+  
 								(fieldUp[2] - fieldDown[2]));  
-}/*
+}
 __global__ void cuda_kernel_jacobi(unsigned char *pressure, unsigned char *divergence, size_t pitch, size_t pitchSlice){  
+	int xIter = blockIdx.x*blockDim.x + threadIdx.x;
+	int yIter = blockIdx.y*blockDim.y + threadIdx.y;
+	int zIter = 0;
+	unsigned char* cellDivergence = divergence + (zIter*pitchSlice) + (yIter*pitch) + (4*xIter);
 	// Get the divergence at the current cell.  
-	float dCentre = divergence.Sample(samPointClamp, in.CENTERCELL);  
+	float dCentre = cellDivergence[0];  
 	// Get pressure values from neighboring cells.  
-	float pLeft = pressure.Sample(samPointClamp, in.LEFTCELL);  
-	float pRight = pressure.Sample(samPointClamp, in.RIGHTCELL);  
-	float pBottom = pressure.Sample(samPointClamp, in.BOTTOMCELL);  
-	float pTop = pressure.Sample(samPointClamp, in.TOPCELL);  
-	float pDown = pressure.Sample(samPointClamp, in.DOWNCELL);  
-	float pUp = pressure.Sample(samPointClamp, in.UPCELL);  
-	// Compute the new pressure value for the center cell.  
-	return(pLeft + pRight + pBottom + pTop + pUp + pDown - dCentre) / 6.0;  
-} 
+	unsigned char *pLeft = pressure + (zIter*pitchSlice) + (yIter*pitch) + (4*(xIter-1));
+	unsigned char *pRight = pressure + (zIter*pitchSlice) + (yIter*pitch) + (4*(xIter+1));
+	unsigned char *pBottom = pressure + ((zIter+1)*pitchSlice) + (yIter*pitch) + (4*xIter);
+	unsigned char *pTop = pressure + ((zIter-1)*pitchSlice) + (yIter*pitch) + (4*xIter);
+	unsigned char *pDown = pressure + (zIter*pitchSlice) + ((yIter-1)*pitch) + (4*xIter);  
+	unsigned char *pUp = pressure + (zIter*pitchSlice) + ((yIter+1)*pitch) + (4*xIter); 
+
+	// Compute the new pressure value for the center cell.
+	unsigned char* cellPressure = pressure + (zIter*pitchSlice) + (yIter*pitch) + (4*xIter);
+	cellPressure[0] = (pLeft[0] + pRight[0] + pBottom[0] + pTop[0] + pUp[0] + pDown[0] - dCentre) / 6.0;  
+} /*
 __global__ void cuda_kernel_project(unsigned char *pressure, unsigned char* velocityInput, size_t pitch, size_t pitchSlice){  
 	// Compute the gradient of pressure at the current cell by  
 	// taking central differences of neighboring pressure values.  
