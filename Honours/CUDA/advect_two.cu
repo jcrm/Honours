8c420bc2e8d490abc3a84fb325f273be22e19bb2
jakemorey@hotmail.co.uk <ctm5010>
2014-04-15 12:50:12 +0100

    updated advect

--- Honours/CUDA/advect_two.cu
+++ Honours/CUDA/advect_two.cu
@@ -15,24 +15,47 @@ __global__ void cuda_kernel_advect_two_texture(unsigned char *output, unsigned c
 		if(x_iter +1 < size_WHD.x && x_iter - 1 > 0){
 			if(y_iter + 1 < size_WHD.y && y_iter - 1 > 0){
 				if(z_iter + 1 < size_WHD.z && z_iter - 1 > 0){
-					//location is z slide + y position + variable size time x position
-					unsigned char *current_velocity = input + (z_iter*pitch_slice) + (y_iter*pitch) + (4*x_iter);
+					unsigned char *fieldRight = input + (z_iter*pitch_slice) + (y_iter*pitch) + (4*(x_iter+1));
+					unsigned char *fieldDown = input + (z_iter*pitch_slice) + ((y_iter+1)*pitch) + (4*x_iter); 
+					unsigned char *fieldRightCorner = input + (z_iter*pitch_slice) + ((y_iter+1)*pitch) + (4*(x_iter+1));
+					unsigned char *field = input + (z_iter*pitch_slice) + (y_iter*pitch) + (4*x_iter);
+
+					unsigned char *fieldRightBack = input + ((z_iter+1)*pitch_slice) + (y_iter*pitch) + (4*(x_iter+1));
+					unsigned char *fieldDownBack = input + ((z_iter+1)*pitch_slice) + ((y_iter+1)*pitch) + (4*x_iter); 
+					unsigned char *fieldRightCornerBack = input + ((z_iter+1)*pitch_slice) + ((y_iter+1)*pitch) + (4*(x_iter+1));
+					unsigned char *fieldBack = input + ((z_iter+1)*pitch_slice) + (y_iter*pitch) + (4*x_iter);
+
+					float temp_X_1 = field[0] +((fieldRight[0]-field[0])*0.5f);
+					float temp_Y_1 = field[1] +((fieldRight[1]-field[1])*0.5f);
+					float temp_Z_1 = field[2] +((fieldRight[2]-field[2])*0.5f);
+
+					float temp_X_2 = fieldDown[0] +((fieldRightCorner[0]-fieldDown[0])*0.5f);
+					float temp_Y_2 = fieldDown[1] +((fieldRightCorner[1]-fieldDown[1])*0.5f);
+					float temp_Z_2 = fieldDown[2] +((fieldRightCorner[2]-fieldDown[2])*0.5f);
+
+					float temp_X_3 = fieldBack[0] +((fieldRightBack[0]-fieldBack[0])*0.5f);
+					float temp_Y_3 = fieldBack[1] +((fieldRightBack[1]-fieldBack[1])*0.5f);
+					float temp_Z_3 = fieldBack[2] +((fieldRightBack[2]-fieldBack[2])*0.5f);
+
+					float temp_X_4 = fieldDownBack[0] +((fieldRightCornerBack[0]-fieldDownBack[0])*0.5f);
+					float temp_Y_4 = fieldDownBack[1] +((fieldRightCornerBack[1]-fieldDownBack[1])*0.5f);
+					float temp_Z_4 = fieldDownBack[2] +((fieldRightCornerBack[2]-fieldDownBack[2])*0.5f);
+
+					temp_X_1 =(temp_X_1 + (temp_X_2-temp_X_1)*0.5f);
+					temp_Y_1 =(temp_Y_1 + (temp_Y_2-temp_Y_1)*0.5f);
+					temp_Z_1 =(temp_Z_1 + (temp_Z_2-temp_Z_1)*0.5f);
+
+					temp_X_3 =(temp_X_3 + (temp_X_4-temp_X_3)*0.5f);
+					temp_Y_3 =(temp_Y_3 + (temp_Y_4-temp_Y_3)*0.5f);
+					temp_Z_3 =(temp_Z_3 + (temp_Z_4-temp_Z_3)*0.5f);
+
 					unsigned char *output_velocity = output + (z_iter*pitch_slice) + (y_iter*pitch) + (4*x_iter);
-
-					float pos_x = float((x_iter +0.5f)- (timeStep * signed int(current_velocity[0])))/ size_WHD.x;
-					float pos_y = float((y_iter +0.5f) - (timeStep * signed int(current_velocity[1])))/ size_WHD.y;
-					float pos_z = float((z_iter +0.5f) - (timeStep * signed int(current_velocity[2]))+0.5f)/ size_WHD.z;
-
-					unsigned int location = (pos_z*pitch_slice) + (pos_y*pitch) + (4*pos_x);
-					current_velocity = input + location;
-
-					output_velocity[0] = signed int(current_velocity[0]);
-					output_velocity[1] = signed int(current_velocity[1]);
-					output_velocity[2] = signed int(current_velocity[2]);
+					output_velocity[0] = signed int(temp_X_1 + ((temp_X_3-temp_X_1)*0.5f));
+					output_velocity[1] = signed int(temp_Y_1 + ((temp_Y_3-temp_Y_1)*0.5f));
+					output_velocity[2] = signed int(temp_Z_1 + ((temp_Z_3-temp_Z_1)*0.5f));
 				}
 			}
 		}
-		
 	}
 }
 
