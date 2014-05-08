eef4bcf3c415a74ec2e34c0c304fffb88d8fe0a3
jakemorey@hotmail.co.uk <ctm5010>
2014-05-08 21:58:13 +0100

    finished flattern need to merge into one value

--- Honours/CUDA/inital _float.cu
+++ Honours/CUDA/inital _float.cu
@@ -16,7 +16,11 @@ __global__ void cuda_kernel_initial_float(float *input, Size size, float value){
 	for(z_iter = 0; z_iter < size.depth_; ++z_iter){ 
 		//location is z slide + y position + variable size time x position
 		float *cell_value = input + (z_iter*size.pitch_slice_) + (y_iter*size.pitch_) + (PIXEL_FMT_SIZE_RG * x_iter);
-		cell_value[0] = value;
+		if(x_iter <= size.width_/2){
+			cell_value[0] = value-30;
+		}else{
+			cell_value[0] = value;
+		}
 		cell_value[1] = 0.f;
 	}
 }
